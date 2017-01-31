fc12d47a4a73f162acb1ba002a10f0cbb8252d71
David Hu <davidhu314@gmail.com>
2017-01-31 13:20:52 -0800

    add firebase config

--- src/components/app.react.js
+++ src/components/app.react.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import {
-  Text,
-  View
-} from 'react-native';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
 
 import Routes from './routes.react';
 
 class App extends React.Component {
+
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyDOCdS9Ho5MluRHsqABjnIRjO0dZW03tjs",
+      authDomain: "eezee-33067.firebaseapp.com",
+      databaseURL: "https://eezee-33067.firebaseio.com",
+      storageBucket: "eezee-33067.appspot.com",
+      messagingSenderId: "904163115748"
+    };
+    firebase.initializeApp(config);
+  }
 
   render() {
     return (
