e0c37c06bd2d50845af99fc8febc162f141e0f06
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-02-02 11:52:46 -0800

    Add query reducer skeleton

---
+++ src/reducers/queries_reducer.js
@@ -0,0 +1,20 @@
+import merge from 'lodash/merge';
+import { RECEIVE_QUERY } from '../actions/query_actions';
+
+const _defaultState = {
+  //TODO add default state
+};
+
+const queriesReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_QUERY:
+      return merge({}, {
+        //TODO update state
+      });
+    default:
+      return state;
+  }
+};
+
+export default queriesReducer;
