51f73016345f460d9a41aebf670dd3159f0ac629
Alex Sherman <asherman.ca@gmail.com>
2017-02-04 14:58:27 -0800

    removed home button

--- src/components/profile/profile.react.js
+++ src/components/profile/profile.react.js
@@ -9,9 +9,7 @@ import Navbar from '../navbar/navbar.react';
 class Profile extends React.Component {
   constructor() {
     super();
-
     this.state = {
-
     };
   }
 
@@ -25,8 +23,14 @@ class Profile extends React.Component {
     return (
       <View style={ styles.viewStyle }>
         <Navbar />
-        <Text style={ styles.usernameStyle }>Test</Text>
-        <Text style={ styles.textStyle }></Text>
+        <View style={ styles.containerStyle }>
+          <Text style={{fontSize: 20, color: '#3B5998', alignSelf: 'center', fontWeight: '600'}}>
+            Welcome to eZ
+          </Text>
+          <Text style={{fontSize: 15, color: '#3B5998', padding: 5}}>
+            The simplest tool for finding the movie you want to stream online. Create an account or start your search.
+          </Text>
+        </View>
         <Footer />
       </View>
     );
@@ -37,6 +41,20 @@ const styles = {
   viewStyle: {
     justifyContent: 'space-between',
     flex: 1
+  },
+  containerStyle: {
+    marginLeft: 25,
+    marginRight: 25,
+    padding: 25,
+    backgroundColor: '#F8F8F8',
+    height: 250,
+    shadowColor: 'rgba(0, 0, 0, 0.12)',
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 2,
+    }
   },
   usernameStyle: {
 
